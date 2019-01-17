bb0463c2dae5dda0c9d09de78f216c76b5d5cc20
Criptdestroyer <ahmademir1201@gmail.com>
2019-01-17 10:58:40 +0700

    backend login and register done

--- application/models/Pengguna_m.php
+++ application/models/Pengguna_m.php
@@ -5,8 +5,8 @@ class Pengguna_m extends MY_Model
 	public function __construct()
 	{
 		parent::__construct();
-		$this->data['table_name']	= 'pengguna';
-		$this->data['primary_key']	= 'id_pengguna';
+		$this->data['table_name']	= 'user';
+		$this->data['primary_key']	= 'id_user';
 	}
 
 	public function login($data)
@@ -15,7 +15,7 @@ class Pengguna_m extends MY_Model
 		if (isset($pengguna))
 		{
 			$this->session->set_userdata([
-				'id_pengguna' 	=> $pengguna->id_pengguna,
+				'id_pengguna' 	=> $pengguna->id_user,
 				'username'		=> $pengguna->username,
 				'id_role'		=> $pengguna->id_role
 			]);
